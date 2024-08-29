22cbaba56ada75b14059d2baa87163992cbd40cd
Iznobad <brandon.cebi.pro@gmail.com>
2024-08-29 15:29:26 +0200

    add new theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#287F71",
+          secondary: "#EB862A",
+          tertiary: "#ABDDD3",
+        },
+        neutral: {
+          light: "#97A3B6",
+          dark: "#111729",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        bold: "700",
+        medium: "500",
+        regular: "400",
+      },
+    },
   },
   plugins: [],
 };
