a43ed41237d9bd7c2a84d92b5d21b9d255e6c0b8
Iznobad <brandon.cebi.pro@gmail.com>
2024-08-30 11:20:11 +0200

    add query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 import { AuthProvider } from "./context/AuthContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </QueryClientProvider>
 );
