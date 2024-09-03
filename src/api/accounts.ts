1871346557896c4e4b3bb3217d009ac148ce603c
Iznobad <brandon.cebi.pro@gmail.com>
2024-09-03 15:17:03 +0200

    Delete accounts

--- src/api/accounts.ts
+++
@@ -1,9 +0,0 @@
-import { Account } from "../types";
-import axiosInstance from "../utils/axiosInstance";
-
-export const fetchAccounts = async (): Promise<Account[]> => {
-  const response = await axiosInstance.get("accounts", {
-    withCredentials: true,
-  });
-  return response.data;
-};
--- src/types/AccountType.ts
+++
@@ -1,5 +0,0 @@
-export interface Account {
-  id: string;
-  name: string;
-  budget: string;
-}
