9231dc9502e86f993c080d9698f40ac7ffb7e0bf
Iznobad <brandon.cebi.pro@gmail.com>
2024-09-03 15:17:55 +0200

    add budgetType

---
+++ src/types/BudgetType.ts
@@ -0,0 +1,5 @@
+export interface RemainingBudgetResponse {
+  remainingBudget: string;
+  totalIncome: string;
+  totalExpense: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,4 @@ export * from "./InputType";
 export * from "./AuthType";
 export * from "./MenuType";
 export * from "./Page";
-export * from "./AccountType";
+export * from "./BudgetType";
